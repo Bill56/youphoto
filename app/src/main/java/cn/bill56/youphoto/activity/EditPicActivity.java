9768d54d0d44221f8e10054018993fc1243fa1ce
Bill56 <yywisme@163.com>
2016-06-15 19:13:36 +0800

    第二次提交，完成了图片编辑界面的编写，实现了点击编辑按钮后的动画效果。

--- app/src/main/java/cn/bill56/youphoto/activity/EditPicActivity.java
+++ app/src/main/java/cn/bill56/youphoto/activity/EditPicActivity.java
@@ -3,12 +3,17 @@ package cn.bill56.youphoto.activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.bill56.youphoto.R;
+import cn.bill56.youphoto.util.AnimatorUtil;
+import cn.bill56.youphoto.util.LogUtil;
 
 
 /**
@@ -17,8 +22,48 @@ import cn.bill56.youphoto.R;
  */
 public class EditPicActivity extends BaseActivity {
 
+    /**
+     * 对应视图中的控件对象
+     */
+    // 工具栏
+    @Bind(R.id.toolBar)
+    Toolbar toolbar;
+    // 操作选项栏布局
+    @Bind(R.id.ll_options)
+    LinearLayout llOptions;
+    // 操作选项栏按钮——编辑
+    @Bind(R.id.btn_pic_edit)
+    Button btnPicEdit;
+    // 操作选项栏按钮——分享
+    @Bind(R.id.btn_pic_share)
+    Button btnPicShare;
+
+    // 图片编辑栏布局
+    @Bind(R.id.ll_edit_actions)
+    LinearLayout llEditActions;
+    // 图片编辑栏布局——旋转
+
+    // 图片编辑栏布局——旗帜
+
+    // 图片编辑栏布局——滤镜
+
+    // 图片编辑栏布局——增强
+
+    // 图片编辑栏布局——涂鸦
+
+
     // 正在编辑的图片
-    private ImageView imgEditingPic;
+    @Bind(R.id.img_editing_pic)
+    ImageView imgEditingPic;
+
+    /**
+     *临时存储变量值的临时成员变量
+     */
+    // 动画作用视图的高度
+    private int mHiddenViewMeasuredHeight;
+    // 像素密度
+    private float mDensity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,43 +77,79 @@ public class EditPicActivity extends BaseActivity {
      * 初始化绑定的布局视图
      */
     private void initview() {
+        // 通过黄牛刀插件快速绑定控件
+        ButterKnife.bind(this);
+        // 用工具栏代替ActionBar
+        setSupportActionBar(toolbar);
+        // 设置表土为空
+        getSupportActionBar().setTitle("");
         // 绑定控件
         imgEditingPic = (ImageView) findViewById(R.id.img_editing_pic);
         // 获取传递过来的intent中的数据
         String imagePath = getIntent().getStringExtra("EDIT_PIC");
         Bitmap bitmap = null;
         bitmap = BitmapFactory.decodeFile(imagePath);
-        /*// 压缩图片
-        Bitmap newBitmap = compressImage(bitmap);*/
+        // 显示需要编辑的图片
         imgEditingPic.setImageBitmap(bitmap);
+        // 为显示动画做准备
+        // 获取像素密度
+        mDensity = getResources().getDisplayMetrics().density;
+        // 获取布局的高度
+        mHiddenViewMeasuredHeight = (int) (mDensity * 80 + 0.5);
+        // 注册事件监听
+        registerListener();
     }
 
     /**
-     * 图片质量压缩,保证图片大小小于100kb
-     *
-     * @param image 原位图
-     * @return 压缩后的位图
+     * 为活动中的控件注册事件监听
      */
-    private Bitmap compressImage(Bitmap image) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        // 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
-        image.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        int options = 100;
-        // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
-        while (baos.toByteArray().length / 1024 > 100) {
-            // 重置baos即清空baos
-            baos.reset();
-            // 这里压缩options%，把压缩后的数据存放到baos中
-            image.compress(Bitmap.CompressFormat.PNG, options, baos);
-            // 每次都减少10
-            options -= 10;
-        }
-        // 把压缩后的数据baos存放到ByteArrayInputStream中
-        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
-        // 把ByteArrayInputStream数据生成图片
-        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);
-        return bitmap;
+    private void registerListener() {
+        // 创建按钮点击事件监听器
+        OnButtonClickListener buttonClickListener = new OnButtonClickListener();
+        btnPicEdit.setOnClickListener(buttonClickListener);
+        btnPicShare.setOnClickListener(buttonClickListener);
     }
 
+    /**
+     * 自定义的按钮点击事件监听器
+     */
+    class OnButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            // 通过id判断点击的是哪个按钮
+            switch (v.getId()) {
+                // 点击的是编辑按钮
+                case R.id.btn_pic_edit:
+                    // 开启动画，让操作栏消失，编辑栏显示
+                    LogUtil.d(LogUtil.TAG,"点击了edit");
+                    llOptions.setVisibility(View.GONE);
+                    AnimatorUtil.animateOpen(llEditActions, mHiddenViewMeasuredHeight);
+                    break;
+                case R.id.btn_quit:
+                    // 调用按下返回键的方法
+                    onBackPressed();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+    }
+
+    /**
+     * 按下返回键的时候执行
+     */
+    @Override
+    public void onBackPressed() {
+        // 当操作栏消失的时候
+        if (llOptions.getVisibility() == View.GONE) {
+            // 调用关闭源动画效果
+            AnimatorUtil.animateClose(llEditActions);
+            // 开启新动画
+            AnimatorUtil.animateOpen(llOptions, mHiddenViewMeasuredHeight);
+        }
+    }
 
 }
---
+++ app/src/main/java/cn/bill56/youphoto/util/AnimatorUtil.java
@@ -0,0 +1,72 @@
+package cn.bill56.youphoto.util;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 封装了一系列动画效果的工具类
+ * Created by Bill56 on 2016/6/15.
+ */
+public class AnimatorUtil {
+
+    /**
+     * 创建下拉动画的方法
+     *
+     * @param view  作用的视图对象
+     * @param start 开始位置
+     * @param end   结束为止
+     * @return 动画对象
+     */
+    public static ValueAnimator createDropAnimator(
+            final View view, int start, int end) {
+        ValueAnimator animator = ValueAnimator.ofInt(start, end);
+        animator.addUpdateListener(
+                new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        int value = (Integer) valueAnimator.getAnimatedValue();
+                        ViewGroup.LayoutParams layoutParams =
+                                view.getLayoutParams();
+                        layoutParams.height = value;
+                        view.setLayoutParams(layoutParams);
+                    }
+                });
+        return animator;
+    }
+
+    /**
+     * 开启缓慢升起的动画
+     *
+     * @param view                     作用的视图对象
+     * @param hiddenViewMeasuredHeight 动画执行的高度
+     */
+    public static void animateOpen(final View view, int hiddenViewMeasuredHeight) {
+        view.setVisibility(View.VISIBLE);
+        ValueAnimator animator = createDropAnimator(
+                view,
+                0,
+                hiddenViewMeasuredHeight);
+        animator.start();
+    }
+
+    /**
+     * 关闭缓慢升起的动画
+     *
+     * @param view 作用的视图对象
+     */
+    public static void animateClose(final View view) {
+        int origHeight = view.getHeight();
+        ValueAnimator animator = createDropAnimator(view, origHeight, 0);
+        animator.addListener(new AnimatorListenerAdapter() {
+            public void onAnimationEnd(Animator animation) {
+                view.setVisibility(View.GONE);
+            }
+        });
+        animator.start();
+    }
+
+
+}
