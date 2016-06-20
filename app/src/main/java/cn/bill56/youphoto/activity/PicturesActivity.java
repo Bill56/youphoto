6dd79a57c58889d1d4e3eddb0d1d00653f28b6f0
Bill56 <yywisme@163.com>
2016-06-20 13:07:27 +0800

    第十三次提交，新增显示所有已修改作品列表查看器，点击作品项可直接跳入编辑界面的功能。

--- app/src/main/java/cn/bill56/youphoto/activity/PicturesActivity.java
+++ app/src/main/java/cn/bill56/youphoto/activity/PicturesActivity.java
@@ -1,11 +1,19 @@
 package cn.bill56.youphoto.activity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.bill56.youphoto.R;
+import cn.bill56.youphoto.adapter.PictureAdapter;
 
 /**
  * 承载显示用该软件修改保存的图片
@@ -15,6 +23,12 @@ public class PicturesActivity extends BaseActivity {
 
     // 工具栏
     private Toolbar toolbar;
+    // 列表视图
+    private RecyclerView recyclerViewPictures;
+    // 绑定的视图的适配器
+    private PictureAdapter pictureAdapter;
+    // 数据
+    private List<File> imgFiles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +48,53 @@ public class PicturesActivity extends BaseActivity {
         // 设置顶部返回键
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.activity_pictures_title);
+        // 绑定可回收列表视图
+        recyclerViewPictures = (RecyclerView) findViewById(R.id.recyclerView_pictures);
+        // 设置列表的布局管理器
+        recyclerViewPictures.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true));
+        // 获得数据列表
+        getImageFiles();
+        // 图片文件夹存在，且有修改的图片
+        if (imgFiles != null && imgFiles.size() > 0) {
+            // 让适配器装载数据
+            pictureAdapter = new PictureAdapter(this,imgFiles);
+            // 设置适配器
+            recyclerViewPictures.setAdapter(pictureAdapter);
+        } else {
+            // 显示图片不存在的视图
+
+        }
+
+    }
+
+    /**
+     * 获得已经修改的图片列表
+     */
+    private void getImageFiles() {
+        // 获取设备的根目录
+        File sdDir = Environment.getExternalStorageDirectory();
+        // 创建缓存目录
+        File cacheDir = new File(sdDir, "UPhotocache");
+        // 如果存在目录，则读取
+        if (cacheDir.exists()) {
+            // 创建数据列表
+            if (imgFiles == null)
+                imgFiles = new ArrayList<>();
+            // 获取cacheDir的所有子文件，除去output_image.jpg
+            // 获得目录中的所有文件
+            File[] files = cacheDir.listFiles();
+            // 遍历文件夹，获取资源
+            for (File imgFile : files) {
+                // 如果当前图片文件的名字不是output_image.jpg，不区分大小写
+                if (!"output_image.jpg".equalsIgnoreCase(imgFile.getName())) {
+                    imgFiles.add(imgFile);
+                }
+            }
+        } else {
+            // 不做任何处理
+        }
+
+
     }
 
     /**
@@ -45,7 +106,7 @@ public class PicturesActivity extends BaseActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // 加载菜单布局
-        getMenuInflater().inflate(R.menu.pictures_option,menu);
+        getMenuInflater().inflate(R.menu.pictures_option, menu);
         return true;
     }
 
---
+++ app/src/main/java/cn/bill56/youphoto/adapter/PictureAdapter.java
@@ -0,0 +1,149 @@
+package cn.bill56.youphoto.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.List;
+
+import cn.bill56.youphoto.R;
+import cn.bill56.youphoto.activity.EditPicActivity;
+
+/**
+ * 跟显示图片的可回收视图绑定的适配器类
+ * Created by Bill56 on 2016/6/20.
+ */
+public class PictureAdapter extends RecyclerView.Adapter<PictureAdapter.ViewHolder> {
+
+    // 上下文环境
+    private Context mContext;
+    // 布局加载器
+    private LayoutInflater inflater;
+    // 数据列表
+    private List<File> mData;
+
+    /**
+     * 构造方法
+     *
+     * @param context 上下文环境
+     * @param data    绑定的数据列表
+     */
+    public PictureAdapter(Context context, List<File> data) {
+        // 初始化成员变量
+        mContext = context;
+        mData = data;
+        inflater = LayoutInflater.from(context);
+    }
+
+    /**
+     * 创建视图项
+     *
+     * @param parent   父元素
+     * @param viewType 视图类系
+     * @return 视图项
+     */
+    @Override
+    public ViewHolder onCreateViewHolder(
+            ViewGroup parent,
+            int viewType) {
+        if (viewType == 1) {
+
+        } else if (viewType == 2) {
+
+        } else {
+
+        }
+        View v = inflater.inflate(R.layout.card_picture, parent, false);
+        return new ViewHolder(v);
+    }
+
+    /**
+     * 绑定视图项的数据
+     *
+     * @param holder   视图项
+     * @param position 位置
+     */
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // 获得文件对象的引用
+        File f = mData.get(position);
+        // 使用毕加索第三方包装载图片
+        Picasso.with(mContext)
+                .load(f.getAbsoluteFile())
+                .error(R.drawable.ic_image_24dp)
+                .into(holder.imgFileIcon);
+        // 绑定图片文件名
+        holder.textFileName.setText(f.getName());
+        // 绑定图片文件大小
+        holder.textFileSize.setText(String.valueOf(f.length()));
+        // 绑定文件路径
+        holder.textFilePath.setText(f.getAbsolutePath());
+    }
+
+    /**
+     * 获得数据的总数
+     *
+     * @return 数据总数
+     */
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+
+    /**
+     * ViewHolder类，用于管理回收资源
+     *
+     * @author Bill56
+     */
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        // 图片缩略图
+        ImageView imgFileIcon;
+        // 图片文件名
+        TextView textFileName;
+        // 图片文件大小
+        TextView textFileSize;
+        // 图片文件路径
+        TextView textFilePath;
+
+        /**
+         * 构造方法
+         *
+         * @param itemView 视图项
+         */
+        public ViewHolder(final View itemView) {
+            super(itemView);
+            // 绑定视图对象
+            imgFileIcon = (ImageView) itemView.findViewById(R.id.image_file_icon);
+            textFileName = (TextView) itemView.findViewById(R.id.text_file_name);
+            textFileSize = (TextView) itemView.findViewById(R.id.text_file_size);
+            textFilePath = (TextView) itemView.findViewById(R.id.text_file_path);
+            itemView.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(mContext, "点击" + textFileName.getText(), Toast.LENGTH_SHORT).show();
+                            // 跳转到图片编辑界面
+                            Intent editPicIntent = new Intent(mContext, EditPicActivity.class);
+                            // 将被点击的图片路径存入
+                            editPicIntent.putExtra("EDIT_PIC",textFilePath.getText());
+                            mContext.startActivity(editPicIntent);
+                        }
+                    }
+            );
+        }
+    }
+
+
+}
